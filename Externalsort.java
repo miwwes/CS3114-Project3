459c4bbf45ca5a45013fd5ec7e0f0ec4e7829f8b
abbym1 <abbym1@vt.edu>
2019-03-21 14:50:02 -0400

    main class added

---
+++ Externalsort.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author abbym1
+ * @author juliam8
+ * 
+ *      Main class for CS3114 Project 2
+ *
+ */
+public class Externalsort {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
