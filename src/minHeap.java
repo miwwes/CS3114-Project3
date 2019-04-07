c7a69e36713fec242b4542f3ac0e5fd22c2becbd
Julia Mahon Kuzin <juliam8@vt.edu>
2019-04-06 21:35:29 -0400

    Dsf

--- src/minHeap.java
+++ src/minHeap.java
@@ -31,12 +31,11 @@ public class minHeap {
         buildHeap(); 
     }
     // Constructor supporting preloading of arr contents
-    minHeap(){ 
+    minHeap() { 
         arr = new byte[4096];  
         n = 4096;  
         size = 4096;
     }
-
     
     // Return current number of records in the arr
     public byte[] getRecord(int pos) {
@@ -82,104 +81,83 @@ public class minHeap {
         return n == 0;
     }
 
-
-    // Return position for left child of pos
     /**
-     * @param pos
-     * @return
+     * Return position for left child of pos
+     * @param pos the position to return the left child of
+     * @return the position of the left child
      */
     public int leftchild(int pos) {
-        if ((pos/16) >= n / 2)
+        if ((pos/16) >= n / 2) {
             return -1;
+        }
         return (2 * pos) + 16;
     }
 
-
-    // Return position for right child of pos
     /**
-     * @param pos
-     * @return
+     * Return position for right child of pos
+     * @param pos the position to return the right child of
+     * @return the position of the right child
      */
     public int rightchild(int pos) {
-        if ((pos/16) >= (n - 1) / 2)
+        if ((pos/16) >= (n - 1) / 2) {
             return -1;
+        }
         return (2 * pos) + 32;
     }
 
-
-    // Return position for parent
     /**
-     * @param pos
-     * @return
+     * Return position for parent
+     * @param pos the position to return the parent of
+     * @return the index of the parent
      */
     public int parent(int pos) {
-        if ((pos/16) <= 0)
+        if ((pos/16) <= 0) {
             return -1;
+        }
         return (pos/16 - 1) / 2;
     }
 
-
-    // Insert val into arr
     /**
-     * @param key
-     */
-    /*public void insert(byte[] key) {
-        if (n*16 >= size) {
-            System.out.println("Heap is full");
-            return;
-        }
-        int curr = n*16;
-        n++;
-        System.arraycopy(key, 0, arr, curr, 16);
-        // Now sift up until curr's parent's key < curr's key
-        byte a[] = Arrays.copyOfRange(arr, curr, curr + 16);
-        byte b[] = Arrays.copyOfRange(arr, parent(curr), parent(curr) + 16);
-        while ((curr != 0) && (compareRecords(a, b) < 0)) {
-            swap(curr, parent(curr));
-            curr = parent(curr);
-        }
-    }*/
-
-
-    // Heapify contents of heap
-    /**
-     * 
+     * Heapify contents of heap
      */
     public void buildHeap() {
-        for (int i = (n / 2 - 1)*16; i >= 0; i-=16)
+        for (int i = (n / 2 - 1) * 16; i >= 0; i -= 16) {
             siftdown(i);
+        }
     }
     
     /**
-     * parameterized
+     * Heapify contents of heap of a specified size
+     * @param size the size of the heap to build
      */
     public void buildHeap(int size) {
         n = size;
-        for (int i = (n / 2 - 1)*16; i >= 0; i -= 16)
+        for (int i = (n / 2 - 1)*16; i >= 0; i -= 16) {
             siftdown(i);
+        }
     }
 
 
-    // Put element in its correct place
     /**
-     * @param pos
+     * Put element in its correct place in heap
+     * @param pos the current position to check for sifting down
      */
     private void siftdown(int pos) {
-        if ((pos < 0) || ((pos/16) >= n))
+        if ((pos < 0) || ((pos/16) >= n)) {
             return; // Illegal position
+        }
         while (!isLeaf(pos)) {
             int j = leftchild(pos);
-            //if (rightchild(pos) != -1) {
             byte a[] = Arrays.copyOfRange(arr, j, j + 16);
             byte b[] = Arrays.copyOfRange(arr, j + 16, j + 32);
-            
             if (((j/16) < (n - 1)) && (compareRecords(a, b) >= 0)) {
                 j += 16; // j is now index of child with lesser value
             }
             byte c[] = Arrays.copyOfRange(arr, pos, pos + 16);
             byte d[] = Arrays.copyOfRange(arr, j, j + 16);
-            if (compareRecords(c, d) <= 0)
+            if (compareRecords(c, d) <= 0) {
                 return;
+            }
             swap(j, pos);
             pos = j; // Move down
             
@@ -187,9 +165,9 @@ public class minHeap {
     }
     
     /**
-     * @param arr2
-     * @param curr
-     * @param parent
+     * Swaps the values in two positions
+     * @param pos1 the first position to swap
+     * @param pos2 the second position to swap
      */
     private void swap(int pos1, int pos2) {
         byte[] temp = Arrays.copyOfRange(arr, pos1, pos1 + 16);
@@ -205,25 +183,28 @@ public class minHeap {
         }  
     }
 
-
-    // Remove and return maximum value
     /**
-     * @return
+     * Remove and return minimum value
+     * @return the minimum value of the heap
      */
     public byte[] removemin() {
-        if (n == 0)
+        if (n == 0) {
             return null; // Removing from empty arr
+        }
         swap(0, (--n) * 16); // Swap maximum with last value
-        if (n != 0) // Not on last element
+        if (n != 0) {// Not on last element
             siftdown(0); // Put new arr root val in correct place
+        }
         
         return Arrays.copyOfRange(arr, n*16, n*16 + 16);
     }
     
-    //toNumber(Arrays.copyOfRange(arr, n*16, n*16 + 16));
-    //toNumber(Arrays.copyOfRange(arr, (n*16)-16, n*16));
-    //toNumber(Arrays.copyOfRange(arr, 16, 32));
-    
+
+    /**
+     * Remove the minimum value and replace with array b
+     * @param b the value to replace the minimum with
+     * @return the minimum value in byte array form
+     */
     public byte[] removemin(byte[] b) {
         if (n == 0) {
             return null; // Removing from empty arr
@@ -235,43 +216,44 @@ public class minHeap {
         System.arraycopy(b, 0, arr, n*16, 16);
         return Arrays.copyOfRange(arr, n*16, n*16 + 16);
     }
-    
-    // return minimum value
-    /**
-     * @return
-     */
-    //public byte[] getMin() {
-    //    return Arrays.copyOfRange(arr, 0, 16);
-    //}
 
-
-    // Modify the value at the given position
     /**
-     * @param pos
-     * @param newVal
+     * Modify the value at the given position
+     * @param pos the position to place the newVal at
+     * @param newVal the new value to insert
      */
     public void modify(int pos, byte[] newVal) {
-      if ((pos < 0) || (pos >= n * 16)) return; // Illegal heap position
+      if ((pos < 0) || (pos >= n * 16)) {
+          return; // Illegal heap position
+      }
       System.arraycopy(newVal, 0, arr, pos, 16);
       update(pos);
     }
 
-
-    // The value at pos has been changed, restore the arr property
     /**
-     * @param pos
+     * The value at pos has been changed, so restore the heap
+     * @param pos the position that has the changed value
      */
     private void update(int pos) {
       // If it is a big value, push it up
       while ((pos > 0) && (compareRecords(
                       Arrays.copyOfRange(arr, pos, pos + 16), 
                       Arrays.copyOfRange(arr, parent(pos), parent(pos) + 16)) < 0)) {
-        swap(pos, parent(pos));
-        pos = parent(pos);
+          swap(pos, parent(pos));
+          pos = parent(pos);
       }
-      if (n != 0) siftdown(pos); // If it is little, push down
+      if (n != 0) {
+          siftdown(pos); // If it is little, push down
+      }
     }
     
+    /**
+     * Helper function that takes a long and a double and returns a
+     * byte array
+     * @param id the id 
+     * @param key
+     * @return
+     */
     public byte[] toByteArray(long id, double key) {
         byte[] bytes1 = new byte[8];
         byte[] bytes2 = new byte[8];
